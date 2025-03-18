dfd0784439273a7e1aaff8447a931827edb39af4
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-03-18 01:23:26 -0400

    views completed

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -13,15 +13,18 @@ function viewChanger() {
 
 function menuToggler() {
     const menu = document.querySelector("#menu");
-    menu.classList.toggle("show");    
+    const button = document.querySelector("#menu-toggler");
+    menu.classList.toggle("show");   
+    button.classList.toggle("show");   
 }
 
 function schemaColorChanger() {
-    const body = document.querySelector("body");
-    body.classList.toggle('dark');
+    const body = [document.querySelector("footer"), document.querySelector("main")]; //experimental feature
+    body[0].classList.toggle('dark');
+    body[1].classList.toggle('dark');
 }
 
 const schemaColor = document.querySelector("#bg-toggler").addEventListener("click", schemaColorChanger);
-const view = document.querySelector("#view-toggler").addEventListener("click", viewChanger);
+const view = document.querySelector(".view-toggler").addEventListener("click", viewChanger);
 const schema = document.querySelector("#menu-toggler").addEventListener("click", menuToggler);
 
