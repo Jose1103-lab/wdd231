42bec0cce15d24a13be54cf7faf7ab470c70aa2f
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-03-19 02:09:34 -0400

    all done

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,21 +1,22 @@
-function viewChanger() {
-    const grid = document.querySelector("grid");
-    const list = document.querySelector("list");
+function viewChanger(identifier) {
+    const container = document.querySelector("#commerce");
 
-    if (grid.classList.contains("active")) {
-        grid.classList.remove("active");
-        list.classList.add("active");
-    } else {
-        grid.classList.add("active");
-        list.classList.remove("active");
+    if (container.classList.contains("list") && identifier === "g") {
+        container.classList.remove("list");
+        container.classList.add("grid");
+    }
+
+    if (container.classList.contains("grid") && identifier === "l") {
+        container.classList.remove("grid");
+        container.classList.add("list");
     }
 }
 
 function menuToggler() {
     const menu = document.querySelector("#menu");
     const button = document.querySelector("#menu-toggler");
-    menu.classList.toggle("show");   
-    button.classList.toggle("show");   
+    menu.classList.toggle("show");
+    button.classList.toggle("show");
 }
 
 function schemaColorChanger() {
@@ -25,7 +26,36 @@ function schemaColorChanger() {
     body[2].classList.toggle('dark');
 }
 
+async function fetchData() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+        return data;
+}
+
+
 const schemaColor = document.querySelector("#bg-toggler").addEventListener("click", schemaColorChanger);
-const view = document.querySelector(".view-toggler").addEventListener("click", viewChanger);
+const gridView = document.querySelector("#grid").addEventListener("click", () => {viewChanger("g")});
+const listView = document.querySelector("#list").addEventListener("click", () => {viewChanger('l')});
 const schema = document.querySelector("#menu-toggler").addEventListener("click", menuToggler);
 
+fetchData().then(data => {
+    const container = document.querySelector("#commerce");
+    data.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("tile");
+
+        card.innerHTML = `<img src="images/data/${member.image}" alt="${member.name}">
+            <div id="tile-info">
+            <h2>${member.name}</h2>
+            <ul>
+                <li><span class="tile-title">Adrress: </span>${member.address}</li>
+                <li><span class="tile-title">Site: </span>${member.membership_level}</li>
+                <li><span class="tile-title">Phone: </span>${member.phone}</li>
+                <li><span class="tile-title">Email: </span>${member.email}</li>
+                <li><span class="tile-title">Site: </span>${member.website}</li>
+            </ul>
+            </div>
+        `;
+        container.appendChild(card);
+    });
+})
