da311746ddeb71292d6503d3a78b8c6fe5f7b1c2
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-03-20 08:09:18 -0400

    settings

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,21 @@ const filter1 = document.querySelector("#cse").addEventListener("click", () => {
 const filter2 = document.querySelector("#wdd").addEventListener("click", () => { classTagLoader("WDD"); });
 
 
+//testing fetch api and asinc/await functions
+async function fetchData() {
+    const response = await fetch("data/datex.json");
+    const data = await response.json();
+
+    // console.log(data.family[0].name);
+    setTimeout(() => {
+        for (let i = 0; i < data.family.length; i++) {
+            console.log(`NAME: ${data.family[i].name}\nAGE: ${data.family[i].age} \n`);
+        }
+    }, 5000);
+}
+
+fetchData();
+
 fetch("data/datex.json")
     .then(response => response.json())
     .then(data => {
@@ -15,4 +30,3 @@ fetch("data/datex.json")
 
 // const info = fetch("data/datex.json")
 // console.log(info);
-
