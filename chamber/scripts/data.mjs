b7fc8c3a8bb3076aeeb69e84fda93044ddb59a4d
josealbertobritosantana <josealbertobritosantana@gmail.com>
2025-04-07 19:17:00 +0000

    huge changes

--- chamber/scripts/data.mjs
+++ chamber/scripts/data.mjs
@@ -12,7 +12,7 @@ async function fetchWeatherData() {
 // this function will be used to fetch the data from form url
 function urlGetter() {
     const data = new URLSearchParams(window.location.search);
-    
+
     return data;
 }
 
@@ -22,4 +22,66 @@ async function fetchData() {
     return data;
 }
 
-export { fetchWeatherData, urlGetter, fetchData };
+//FIXME: (ft-1003) experimental feature applied to the dicover page
+function factGetter() {
+    fetch("data/fact.json")
+        .then(response => response.json())
+        .then(data => {
+            const factsContainer = document.querySelector(".curiosity");
+            const facts = data.facts.flatMap(category => category.items);
+            const factsLength = facts.length;
+            const factsLimit = Math.min(factsLength, 1); 
+            for (let i = 0; i < factsLength && i < factsLimit; i++) {
+                const newsItem = document.createElement("div");
+                const randomIndex = Math.floor(Math.random() * factsLength);
+                const fact = facts[randomIndex];
+                newsItem.classList.add("curiority-card");
+                newsItem.innerHTML = `
+                    <h4 class="card-curiosity">${fact.fact}</h4>
+                    <img src="${fact.image}" alt="${fact.fact}" loading="lazy">
+                    <p class="card-curiosity">${fact.description}</p>
+                `;
+                factsContainer.appendChild(newsItem);
+            }
+        });
+}
+
+//FIXME: (ft-1002) experimental feature applied to the dicover page
+function newsGetter() {
+    // CORS proxy is used to bypass CORS restrictions when fetching the RSS feed
+    // this module will be migrated to the data.mjs, such file will be handling the fetch request for local news.
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS proxy URL
+    const rssUrl = 'https://www.diariolibre.com/rss/economia.xml';
+    fetch(proxyUrl + rssUrl)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const items = data.querySelectorAll("item");
+            const itemsLength = items.length;
+            const itemsLimit = itemsLength > 3 ? 3 : itemsLength; // Limit to 5 items   
+            let counter = 0;
+            items.forEach(item => {
+                if (counter >= itemsLimit) return;
+                const title = item.querySelector("title").textContent;
+                const link = item.querySelector("link").textContent;
+                const newsContainer = document.querySelector(".news");
+                const newsItem = document.createElement("div");
+                newsItem.classList.add("news-card");
+                const mediaContent = item.getElementsByTagName("media:content")[0];
+                const imageUrl = mediaContent ? mediaContent.getAttribute("url") : "default-image.jpg";
+                newsItem.innerHTML = `
+            <a href="${link}" target="_blank">
+                <div class="news-card-title">
+                <img src="${imageUrl}" alt="News Image" loading="lazy" width="200" height="100">
+                <h4>${title}</h4>Read more</a>
+                </div>
+                `;
+                newsContainer.appendChild(newsItem);
+                // console.log(`Title: ${title}\nLink: ${link}\n`);
+                counter++;
+            });
+        })
+        .catch(err => console.error(`Error fetching RSS (check the "Cors parse" because it needs authorization): ${err}`));
+}
+
+export { fetchWeatherData, urlGetter, fetchData, newsGetter, factGetter };
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,77 +1,18 @@
 import { formData, weatherBuilder, modalHandler, viewChanger, menuToggler, yearLoader, memberInformation, schemaColorChanger } from "./dom-handler.mjs";
+import { newsGetter, factGetter } from "./data.mjs";
 
-const getTitle = document.title; // this is experimental (ft-1001)
 
+newsGetter();
+factGetter();
 yearLoader();
 memberInformation();
 weatherBuilder();
+
 //FIXME: (ft-1001) experimental feature applied to the join page 
+const getTitle = document.title; // this is experimental (ft-1001)
 if (getTitle.includes("Join")) {
     formData();
 }
-
-//FIXME: (ft-1002) experimental feature applied to the dicover page
-// CORS proxy is used to bypass CORS restrictions when fetching the RSS feed
-// this module will be migrated to the data.mjs, such file will be handling the fetch request for local news.
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS proxy URL
-const rssUrl = 'https://www.diariolibre.com/rss/economia.xml';
-
-
-fetch("data/fact.json")
-    .then(response => response.json())
-    .then(data => {
-        const factsContainer = document.querySelector(".curiosity"); 
-        const factsLength = data.length;
-        const factsLimit = factsLength > 1 ? 1 : factsLength; // Limit to 5 items   
-        console.log(data)
-        let counter = 0; 
-        // data.forEach(item => {
-        //     if (counter >= factsLimit) return;
-        //     const title = item.title;
-        //     const link = item.link;
-        //     const newsItem = document.createElement("div");
-        //     newsItem.classList.add("curiority-card"); 
-        //     newsItem.innerHTML = `
-        //         <img src="${item.image}" alt="News Image" loading="lazy" width="200" height="100">
-        //           <a href="${link}" target="_blank">
-        //         <h4>${title}</h4>Read more</a>
-        //         `;
-        //     factsContainer.appendChild(newsItem);
-        //     // console.log(`Title: ${title}\nLink: ${link}\n`);
-        //     counter++; 
-        // });
-    })
-
-fetch(proxyUrl + rssUrl)
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-        const items = data.querySelectorAll("item");
-        const itemsLength = items.length;
-        const itemsLimit = itemsLength > 3 ? 3 : itemsLength; // Limit to 5 items   
-        let counter = 0; 
-        items.forEach(item => {
-            if (counter >= itemsLimit) return;
-            const title = item.querySelector("title").textContent;
-            const link = item.querySelector("link").textContent;
-            const newsContainer = document.querySelector(".news");
-            const newsItem = document.createElement("div");
-            newsItem.classList.add("news-card"); 
-            const mediaContent = item.getElementsByTagName("media:content")[0];
-            const imageUrl = mediaContent ? mediaContent.getAttribute("url") : "default-image.jpg"; 
-            newsItem.innerHTML = `
-            <a href="${link}" target="_blank">
-                <div class="news-card-title">
-                <img src="${imageUrl}" alt="News Image" loading="lazy" width="200" height="100">
-                <h4>${title}</h4>Read more</a>
-                </div>
-                `;
-            newsContainer.appendChild(newsItem);
-            // console.log(`Title: ${title}\nLink: ${link}\n`);
-            counter++; 
-        });
-    })
-    .catch(err => console.error(`Error fetching RSS (check the "Cors parse" because it needs authorization): ${err}`));
 
 const schema = document.querySelector("#menu-toggler").addEventListener("click", menuToggler);
 const schemaColor = document.querySelector("#bg-toggler").addEventListener("click", schemaColorChanger);
@@ -80,7 +21,6 @@ const schemaColor = document.querySelector("#bg-toggler").addEventListener("clic
 const schemaChecker = localStorage.getItem("schema"); // with the feature i will look for the preferred schema color saved in the local storage 
 if (schemaChecker === "dark") { schemaColorChanger(); }
 
-// this module will be migrated to the data.mjs, such file will be handling the fetch request for apis, json, etc
 
 try {
     modalHandler("#gd-modal", 0);
